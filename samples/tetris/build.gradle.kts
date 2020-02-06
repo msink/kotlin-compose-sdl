05543110255941e21bc3f86153b0868cbdc2e81f
Mike Sinkovsky <msink@permonline.ru>
2020-02-06 11:55:11 +0500

    move linkerOpts to :SDL2

--- samples/tetris/build.gradle.kts
+++ samples/tetris/build.gradle.kts
@@ -7,45 +7,17 @@ plugins {
 
 kotlin {
     val os = org.gradle.internal.os.OperatingSystem.current()!!
-    if (os.isLinux) linuxX64("tetris") {
-        binaries {
-            executable {
-                entryPoint = "sample.tetris.main"
-                linkerOpts("-L/usr/lib64", "-L/usr/lib/x86_64-linux-gnu", "-lSDL2")
-            }
-        }
-    }
-
-    if (os.isMacOsX) macosX64("tetris") {
-        binaries {
-            executable {
-                entryPoint = "sample.tetris.main"
-                linkerOpts("-L/opt/local/lib", "-L/usr/local/lib", "-lSDL2")
-            }
-        }
-    }
-
-    if (os.isWindows) mingwX64("tetris") {
-        binaries {
-            executable {
-                entryPoint = "sample.tetris.main"
-                linkerOpts(
-                        "-Wl,-Bstatic",
-                        "-lstdc++",
-                        "-static",
-                        "-limm32",
-                        "-lole32",
-                        "-loleaut32",
-                        "-lversion",
-                        "-lwinmm",
-                        "-lsetupapi",
-                        "-mwindows"
-                    )
-            }
-        }
-    }
+    if (os.isLinux) linuxX64("tetris")
+    if (os.isMacOsX) macosX64("tetris")
+    if (os.isWindows) mingwX64("tetris")
 
     targets.withType<KotlinNativeTarget> {
+        binaries {
+            executable {
+                entryPoint = "sample.tetris.main"
+            }
+        }
+
         sourceSets["${targetName}Main"].apply {
             dependencies {
                 implementation(project(":SDL2"))
